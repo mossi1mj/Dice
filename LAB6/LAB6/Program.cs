bcfba3be9172313e375ece04de3fb24cb76d42ba
mossi1mj <mossjmyron@gmail.com>
2019-10-23 17:01:08 -0400

    third commit

--- LAB6/LAB6/Program.cs
+++ LAB6/LAB6/Program.cs
@@ -6,40 +6,46 @@ namespace LAB6
     {
         static void Main(string[] args)
         {
-            Program.Intro();
-            Console.Write("How many sides should each die have?");
-            int num = int.Parse(Console.ReadLine());
-            var random = new Random();
-            for (int i = 1; i <= 2; i++)
+            bool resumeApp = Program.Second("Welcome to the Grand Circus Casino! Roll the dice? (y/n):\n=========================================================");
+            while (resumeApp)
             {
-                int dice = random.Next(1, num);
-                Console.WriteLine(dice);
-            }
+                Console.Write("How many sides should each die have?");
+                int num = int.Parse(Console.ReadLine());
+                var random = new Random();
+                int dice1 = random.Next(1, num);
+                Console.WriteLine(dice1);
+                int dice2 = random.Next(1, num);
+                Console.WriteLine(dice2);
+                int sum = dice2 + dice1;
 
+                if (dice1 == 1 && dice2 == 1)
+                {
+                    Console.WriteLine("You rolled a Snake Eyes!");
+                }
+                else if (sum == 2 || sum == 3 || sum == 12)
+                {
+                    Console.WriteLine("Craps!");
+                }
+                else if (dice1 == 6 && dice2 == 6)
+                {
+                    Console.WriteLine("Box Cars");
+                }
+
+                resumeApp = Program.Second("Continue? (y/n)");
+            }
         }
-        static void Intro()
+        static bool Second(string prompt)
         {
-            bool continueLoop = true;
-            while (continueLoop)
-            {
-                Console.WriteLine("Welcome to the Grand Circus Casino! Roll the dice? (y/n):");
-                Console.WriteLine("=========================================================");
-                string answer = Console.ReadLine().ToLower();
+            Console.WriteLine(prompt);
+            string answer = Console.ReadLine().ToLower();
 
-                while (answer != "y" && answer != "n")
-                {
-                    Console.Write("Please enter either (y/n): ");
-                    answer = Console.ReadLine().ToLower();
-                }
-                if (answer == "y")
-                {
-                    continueLoop = false;
-                }
-                else if (answer == "n")
-                {
-                    Environment.Exit(0);
-                }
+            while (answer != "y" && answer != "n")
+            {
+                Console.Write("Please enter either (y/n): ");
+                answer = Console.ReadLine().ToLower();
             }
+            return answer == "y";
+            
         }
 
     }
